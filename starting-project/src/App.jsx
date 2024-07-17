fe65a15f97d0cdcc07e902ea7630541f482f85fc
fluffybunny06 <yiphuenzhang@gmail.com>
2024-07-17 15:05:59 +0800

    Update CoreConcept props

--- starting-project/src/App.jsx
+++ starting-project/src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
-import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -25,7 +25,7 @@ function Header() {
 function CoreConcept(props) {
   return (
     <li>
-      <img src={props.img} alt={props.title} />
+      <img src={props.image} alt={props.title} />
       <h3>{props.title}</h3>
       <p>{props.description}</p>
     </li>
@@ -40,13 +40,11 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
-              title="Components"
-              description="The core UI building block."
-              img={componentsImg}
-            />
-            <CoreConcept />
-            <CoreConcept />
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {/* Can also be written as title={CORE_CONCEPTS[0].title} */}
           </ul>
         </section>
         <h2>Time to get started!</h2>
