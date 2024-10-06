98af15328a6cbe46a25f0b2d102af75c62feec71
Yexuan-Zhang <yzha6264@uwo.ca>
2024-10-05 20:48:47 -0400

    Create state in App.jsx to update activePlayerSymbol

--- tic-tac-toe-project/src/App.jsx
+++ tic-tac-toe-project/src/App.jsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
+
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 
 function App() {
+  const [activePlayer, setAcivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setAcivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+  }
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X"></Player>
-          <Player initialName="Player 2" symbol="O"></Player>
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          ></Player>
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          ></Player>
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
     </main>
   );
