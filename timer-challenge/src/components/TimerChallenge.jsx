ccc83aa09f07f226dc95a1dc6a6bededd2d57111
fluffybunny06 <yiphuenzhang@gmail.com>
2025-06-13 22:37:28 -0400

    Expose ResultModal APIs via useImperativeHandle hook

--- timer-challenge/src/components/TimerChallenge.jsx
+++ timer-challenge/src/components/TimerChallenge.jsx
@@ -3,6 +3,7 @@ import ResultModal from "./ResultModal.jsx";
 
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
+  const dialog = useRef();
 
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
@@ -11,6 +12,7 @@ export default function TimerChallenge({ title, targetTime }) {
     setTimerStarted(true);
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.open();
     }, targetTime * 1000);
   }
 
@@ -20,7 +22,7 @@ export default function TimerChallenge({ title, targetTime }) {
 
   return (
     <>
-      {timerExpired && <ResultModal targetTime={targetTime} result="lost" />}
+      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
