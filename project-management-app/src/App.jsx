b0822305dddeb0f1354c10da7a0dd543374134cc
fluffybunny06 <yiphuenzhang@gmail.com>
2025-06-15 18:51:38 -0400

    Tailor styles

--- project-management-app/src/App.jsx
+++ project-management-app/src/App.jsx
@@ -1,9 +1,12 @@
+import NewProject from "./component/NewProject";
+import NoProjectSelected from "./component/NoProjectSelected";
 import ProjectSidebar from "./component/ProjectSidebar";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar />
+      <NoProjectSelected />
     </main>
   );
 }
