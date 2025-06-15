9a10499e44eba3df53fffd928a404f8c0c4b3518
fluffybunny06 <yiphuenzhang@gmail.com>
2025-06-15 19:07:03 -0400

    Manage state to make sure NewProject works between ProjectSidebar and NoProjectSelected

--- project-management-app/src/App.jsx
+++ project-management-app/src/App.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import NewProject from "./component/NewProject";
 import NoProjectSelected from "./component/NoProjectSelected";
 import ProjectSidebar from "./component/ProjectSidebar";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NoProjectSelected />
+      <ProjectSidebar onStartAddProject={handleAddProject} />
+      {content}
     </main>
   );
 }
