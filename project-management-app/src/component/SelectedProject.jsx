0b62f78fb6438911277ff82161183e336f5d6f7b
fluffybunny06 <yiphuenzhang@gmail.com>
2025-06-20 21:41:04 -0400

    Add tasks to a project

--- project-management-app/src/component/SelectedProject.jsx
+++ project-management-app/src/component/SelectedProject.jsx
@@ -1,4 +1,12 @@
-export default function SelectedProject({ project, onDelete }) {
+import Task from "./Task";
+
+export default function SelectedProject({
+  project,
+  onDelete,
+  onAddTask,
+  onDeleteTask,
+  tasks,
+}) {
   const formattedDate = new Date(project.dueDate).toLocaleDateString("en-US", {
     year: "numeric",
     month: "short",
@@ -24,7 +32,7 @@ export default function SelectedProject({ project, onDelete }) {
           {project.description}
         </p>
       </header>
-      TASKS
+      <Task onAdd={onAddTask} onDelete={onDeleteTask} tasks={tasks} />
     </div>
   );
 }
